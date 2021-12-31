4afe0380931d58cd8dbff7bf0c44c5fca16e0a99
Jorge Miguel Werle Júnior <jmwjunior@hotmail.com>
2021-12-31 16:40:38 -0300

    ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log(`O console funcionou corretamente`)
+var vel = 78.2 // condição simples , porquie la nao tem o senão (else)
+console.log(`A velocidade de seu carro é de ${vel} km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade!, Multado`) // este comando só vai acontecer se a velocidade for maiior que 60
+}
+
+console.log(`Dirija sempre com segurança, usando o cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivento em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro!')
+} else {
+    console.log('Você é brasileiro!')
+}
