45a655aff2a59e9a82dec8280ac3cf22f5aac3a7
Jorge Miguel Werle Júnior <jmwjunior@hotmail.com>
2022-01-06 14:49:01 -0300

    ex0016 contador

--- aula14ex/ex0016/script.js
+++ aula14ex/ex0016/script.js
@@ -1,19 +1,32 @@
-function contar() {
-    var iniciarPasso = document.getElementById('txtinicio')
-    var fimPasso = document.getElementById('txtfim')
-    var passo = document.getElementById('txtpasso')
-    var res = document.querySelector('div#res')
+function contar(){
+        let ContIni = window.document.getElementById('txtinicio')
+        let ContFim = window.document.getElementById('txtfim')
+        let p =  window.document.getElementById('txtpasso')
+        let cInicia = Number(ContIni.value)
+        let cFim = Number(ContFim.value)
+        let passo = Number(p.value)
+        let res = window.document.getElementById('res')
+        if (cInicia == '' || cFim == '' || passo == ''){
+                res.innerHTML = `Impossível contar`
+                alert(`Ambos os campos precisam ser preenchidos com um valor a partir de 1`)
+                
+        }else {
+                 res.innerHTML = 'Contando: <br>'
+                if (passo <=0 ){
+                        alert('Passo inválido, vamos considerar o passo como sendo 1')
+                        passo=1
+                }
 
-    if (iniciarPasso.value.length == '' || fimPasso.value.length == '') {
-        alert('[Erro] Insira um número no Início')
-
-    }else {
-        iniciar = Number(iniciarPasso.value)
-        fim = Number(fimPasso.value)
-        passos = Number(passo.value)
-        for (var iniciar; fim <= iniciar ; passos)
+                if (cInicia<cFim){
+                           for(let c = cInicia ; c <= cFim; c+= passo ){
+                        res.innerHTML += ` ${c} \u{1F449}`	
+                        }
+                
+                } else{
+                        for(let c = cInicia; c>= cFim ; c -= passo){
+                                res.innerHTML += `${c} \u{1F449}`
+                        }
+                }  
+                res.innerHTML += `\u{1F3C1}` 
         }
-        res.style.textAling = 'center'
-        res.innerHTML = `Contanto: ${iniciar} de ${passos} em ${passos} até ${fim} `
-            }
 }
