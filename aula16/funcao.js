8ab2b7265ffb10c23269e7d769bd5d8015d1808f
Jorge Miguel Werle Júnior <jmwjunior@hotmail.com>
2022-01-12 18:09:56 -0300

    funcao

---
+++ aula16/funcao.js
@@ -0,0 +1,9 @@
+function parimpar(n){
+    if(n%2==0){
+        return 'É par!'
+    }else{
+        return 'É impar'
+    }
+}
+
+console.log(parimpar(25)) /* Serve para pedir pra mostrar o resoltado desta função*/
