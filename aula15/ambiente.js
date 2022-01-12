18bf9d726e4864a8cd9ac17299f7868d54b6da8d
Jorge Miguel Werle Júnior <jmwjunior@hotmail.com>
2022-01-12 17:16:02 -0300

    array

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,3 +5,10 @@ num.push(1)
 console.log(num)
 console.log(`A minha array tem ${num.length} posições`)
 console.log(`O primeiro valor da array é ${num[0]}`)
+
+let pos = num.indexOf(4)
+if(pos == -1){
+    console.log(`O valor não foi encontrado`)
+}else{
+    console.log(`O valor 4 esta na posiçao ${pos}`)
+}
